1550fa49aaa2ba02e93042a334e83f4206485c83
Juyeon999 <19j@sookmyung.ac.kr>
2021-07-06 21:42:35 +0900

    20210706TUE_JuYeon diary xml only Error; fragment_diary.xml line27 (수정예정)

--- app/src/main/java/com/example/sdetector/ui/diary/DiaryFragment.java
+++ app/src/main/java/com/example/sdetector/ui/diary/DiaryFragment.java
@@ -27,13 +27,7 @@ public class DiaryFragment extends Fragment {
         binding = FragmentDiaryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDiary;
-        diaryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
     }
 
