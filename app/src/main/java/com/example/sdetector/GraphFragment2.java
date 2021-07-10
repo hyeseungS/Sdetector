8bdfbf08146fda3c97ccfcedaff791f21bc1ce21
hyeseung2000 <hyeseung2000@gmail.com>
2021-07-10 12:25:55 +0900

    shinhyeseung mainpage

---
+++ app/src/main/java/com/example/sdetector/GraphFragment2.java
@@ -0,0 +1,23 @@
+package com.example.sdetector;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class GraphFragment2 extends Fragment {
+    public GraphFragment2() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_graph2, container, false);
+        return rootView;
+        //xml 레이아웃이 인플레이트 되고 자바소스 코드와 연결이된다.
+    }
+}
