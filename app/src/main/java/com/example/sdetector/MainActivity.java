e14de38aa4e8d40434ed1d22c1a3e8539e8ed5f3
hyeseung2000 <hyeseung2000@gmail.com>
2021-07-10 12:41:07 +0900

    shinhyeseung mainpage

--- app/src/main/java/com/example/sdetector/MainActivity.java
+++ app/src/main/java/com/example/sdetector/MainActivity.java
@@ -12,7 +12,6 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
