350e46a3b86c330457909c81ff2966d38d37fdd8
yoonyeseul <yoonyesle@naver.com>
2021-07-20 20:44:18 +0900

    kakao_login

--- app/src/main/java/com/example/sdetector/LoginActivity.java
+++ app/src/main/java/com/example/sdetector/LoginActivity.java
@@ -6,6 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.kakao.sdk.auth.model.OAuthToken;
+import com.kakao.sdk.user.UserApiClient;
+import com.kakao.sdk.user.model.User;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function2;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -44,6 +52,52 @@ public class LoginActivity extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(),
                         Login2Activity.class);
                 startActivity(intent);
+            }
+        });
+
+        Function2<OAuthToken, Throwable, Unit> callback = new Function2<OAuthToken, Throwable, Unit>() {
+            @Override
+            public Unit invoke(OAuthToken oAuthToken, Throwable throwable) {
+                if (oAuthToken != null) {
+                    //
+                }
+                if (throwable != null) {
+                    //
+                }
+                checkKakaoLogin();
+                return null;
+            }
+        };
+
+        ImageButton kakaoBtn = (ImageButton) findViewById(R.id.buttonLoginKakao);
+        kakaoBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                if (UserApiClient.getInstance().isKakaoTalkLoginAvailable(LoginActivity.this)) {
+                    UserApiClient.getInstance().loginWithKakaoTalk(LoginActivity.this, callback);
+                }
+                else {
+                    UserApiClient.getInstance().loginWithKakaoAccount(LoginActivity.this, callback);
+                }
+            }
+        });
+
+    }
+
+    private void checkKakaoLogin() {
+        UserApiClient.getInstance().me(new Function2<User, Throwable, Unit>() {
+            @Override
+            public Unit invoke(User user, Throwable throwable) {
+                if (user != null) {
+                    Intent intent = new Intent(getApplicationContext(),
+                            MainActivity.class);
+                    startActivity(intent);
+                }
+                else {
+                    // 로그아웃 상태.
+                }
+                return null;
             }
         });
     }
